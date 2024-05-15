c23ef6a15f1c3c5ed091b6908820744bfcd9a712
Ömer Sungur <omersungur615@gmail.com>
2024-05-16 00:49:02 +0300

    Create ThirdCodelab project

---
+++ Omer/Unit-1/ThirdCodelab/app/src/main/java/com/omersungur/thirdcodelab/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.omersungur.thirdcodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.omersungur.thirdcodelab.ui.theme.ThirdCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThirdCodelabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
